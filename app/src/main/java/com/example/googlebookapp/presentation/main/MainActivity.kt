f445e6b1d49b11739b2d89e5bb9af2502ff5895a
osterekhova <o.s.terekhova@tinkoff.ru>
2020-03-31 12:09:34 +0700

    fix StateStrategyType

--- app/src/main/java/com/example/googlebookapp/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/googlebookapp/presentation/main/MainActivity.kt
@@ -12,8 +12,8 @@ class MainActivity : MvpAppCompatActivity(), BaseView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
         if (savedInstanceState == null) {
-            setSupportActionBar(toolbar)
             displaySearchFragment()
         }
     }
--- app/src/main/java/com/example/googlebookapp/presentation/main/search_book/SearchBookView.kt
+++ app/src/main/java/com/example/googlebookapp/presentation/main/search_book/SearchBookView.kt
@@ -2,10 +2,10 @@ package com.example.googlebookapp.presentation.main.search_book
 
 import com.example.googlebookapp.data.entity.BookEntity
 import com.example.googlebookapp.presentation.common.BaseView
-import moxy.viewstate.strategy.OneExecutionStateStrategy
+import moxy.viewstate.strategy.AddToEndSingleStrategy
 import moxy.viewstate.strategy.StateStrategyType
 
-@StateStrategyType(OneExecutionStateStrategy::class)
+@StateStrategyType(AddToEndSingleStrategy::class)
 interface SearchBookView : BaseView {
 
     fun showData(books: ArrayList<BookEntity>)
